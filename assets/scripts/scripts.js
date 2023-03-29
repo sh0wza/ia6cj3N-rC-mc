8a8f66fd5bb321a44895c2e3e516cbf757dddba0
sh0wza <112489538+sh0wza@users.noreply.github.com>
2023-03-28 21:20:47 -0300

    Add files via upload

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,41 @@
+function loadMore() {
+    $('#more').show()
+    $('.fb-comments-loadmore').hide().remove()
+}
+
+$('like').on('click', function() {
+    if ($(this).hasClass('liked')) {
+        $(this).parents('.fb-comments-wrapper, .fb-comments-reply-wrapper').find('likes').text(parseInt($(this).parents('.fb-comments-wrapper, .fb-comments-reply-wrapper').find('likes').text()) - 1)
+        $(this).removeClass('liked')
+        $(this).text('Curtir')
+    } else {
+        $(this).parents('.fb-comments-wrapper, .fb-comments-reply-wrapper').find('likes').text(parseInt($(this).parents('.fb-comments-wrapper, .fb-comments-reply-wrapper').find('likes').text()) + 1)
+        $(this).addClass('liked')
+        $(this).text('Descurtir')
+    }
+})
+
+function setReturningVisitorCookie() {
+var d = new Date();
+d.setTime(d.getTime() + 1510000);
+var expires = "expires=" + d.toUTCString();
+document.cookie = "returningVisitor=yes;" + expires + ";path=/;max-age=31536000";
+}
+setTimeout(setReturningVisitorCookie, 1510000);
+
+const cookieValue = document.cookie.match('(^|;)\\s*' + 'returningVisitor' + '\\s*=\\s*([^;]+)');
+if (cookieValue) {
+    setTimeout(function() {
+        var elementos = document.getElementsByClassName("add-to-cart");
+        for (var i = 0; i < elementos.length; i++) {
+            elementos[i].style.display = "flex";
+        }
+    }, 0);
+}
+
+setTimeout(function() {
+    var elementos = document.getElementsByClassName("add-to-cart");
+    for (var i = 0; i < elementos.length; i++) {
+        elementos[i].style.display = "flex";
+    }
+}, 1510000);
